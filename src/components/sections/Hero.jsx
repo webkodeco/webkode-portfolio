9225de9af0929156d5eef01fce60c2943fa4519a
carlosesconav <carlosesconav@gmail.com>
2025-09-03 23:30:28 -0500

    feat: update text hero and colors

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -25,11 +25,11 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
-            <h1 className="inline md:hidden">Analytics</h1>
-            <h1 className="hidden md:inline">Analytic tools suite</h1>
+            <h1 className="inline md:hidden">Tecnología empresarial simple,</h1>
+            <h1 className="hidden md:inline">Tecnología empresarial simple,</h1>
           </div>
           <h1 className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide text-white px-8 sm:px-20 md:px-24 lg:px-24">
-            for developers
+             ágil y efectiva
           </h1>
         </motion.div>
         <motion.div
@@ -38,8 +38,8 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <h2 className="text-white text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-            Experience seamless business integrations and powerful insights with
-            our cutting-edge analytic tools.
+           Unifica tus procesos y obtén la información que tu negocio 
+           necesita para avanzar.
           </h2>
         </motion.div>
         <motion.div
