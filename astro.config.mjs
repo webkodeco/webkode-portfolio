0a8ecd3d505073851336ae31b37056a3a8fee519
carlosesconav <carlosesconav@gmail.com>
2025-09-10 22:27:02 -0500

    feat:  upgrade SEO rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "server",
+  site: "https://webkode.co/",
   adapter: cloudflare({}),
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
