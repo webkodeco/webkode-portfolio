7df9ca2eb0e8951ad5c64f2ce4d7a4a1265f14e3
kmvillalobos <kevinmorelov@hotmail.com>
2026-05-04 18:01:21 -0500

    fix: deploy astro as static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://webkode.co",
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
